f4bf5d71d79beac83bc02f9ee863dc96d543bc40
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-22 15:01:25 -0400

    initilized the main backend server file

---
+++ Server/index.js
@@ -0,0 +1,54 @@
+// this is the main backend file that will run the server and connect to the database
+
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const path = require("path");
+const db = require("./config/dbConnection");
+const { expressMiddleware } = require("@apollo/server/express4");
+
+const { typeDefs, resolvers } = require("./schema");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+});
+
+// Define the startApolloServer function
+const startApolloServer = async () => {
+  try {
+    await server.start();
+
+    // Applying required middleware for the application
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use("/graphql", expressMiddleware(server));
+
+    // Serving different files based on the environment
+    if (process.env.NODE_ENV === "production") {
+      app.use(express.static(path.join(__dirname, "../client/dist")));
+
+      app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+      });
+    }
+
+    // Connect to the database and start the Express.js server
+    db.once("open", () => {
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      });
+    });
+  } catch (error) {
+    console.error(
+      "Error starting the Apollo Server or connecting to the database:",
+      error
+    );
+  }
+};
+
+startApolloServer();
