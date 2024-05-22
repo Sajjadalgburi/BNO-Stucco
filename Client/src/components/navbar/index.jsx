5005a7dc5138ae48c942ed0c43b00d99d66c4ae6
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-22 18:31:21 -0400

    refactor: Update navbar component to include links and improve styling

--- Client/src/components/navbar/index.jsx
+++ Client/src/components/navbar/index.jsx
@@ -1,15 +1,23 @@
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100 p-5">
-      <a className="btn btn-ghost text-2xl ml-48">Brothers Exterior Stucoo</a>
+    <div className=" navbar flex justify-between bg-base-100 p-5">
+      <Link to={'/'} className="btn btn-ghost text-2xl ml-48">
+        Brothers <span className="exterior">Exterior</span> Stucoo
+      </Link>
 
-      <ul className="menu menu-horizontal px-1">
+      <ul className="uppercase menu menu-horizontal px-1 mr-24 text-lg font-medium">
         <li>
-          <a>Phone Us</a>
+          <Link>Phone Us</Link>
+        </li>
+
+        <li className="mx-6">
+          <Link to="/">Home</Link>
         </li>
 
         <li>
-          <a>Contact</a>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
     </div>
