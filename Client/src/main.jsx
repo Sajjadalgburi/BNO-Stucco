7a2167db15bed713c251b0b9b5ff8ea60e6ca142
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-22 18:30:13 -0400

    refactor: Update main.jsx to include routing and components for homepage, contact, and error page

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// components
+import HomePage from './pages/homepage';
+import Contact from './components/contact/index.jsx';
+import ErrorPage from '../src/pages/ErrorPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    errorElement: <ErrorPage />,
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />,
 );
