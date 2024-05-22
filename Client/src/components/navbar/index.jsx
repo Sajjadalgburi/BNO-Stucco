039c441a602faba7c7e1bcae97101b2164e08aa6
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-22 17:04:19 -0400

    feat: Add navbar component

---
+++ Client/src/components/navbar/index.jsx
@@ -0,0 +1,17 @@
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100 p-5">
+      <a className="btn btn-ghost text-2xl ml-48">Brothers Exterior Stucoo</a>
+
+      <ul className="menu menu-horizontal px-1">
+        <li>
+          <a>Phone Us</a>
+        </li>
+
+        <li>
+          <a>Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
