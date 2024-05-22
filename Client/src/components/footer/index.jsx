4bab3402d46a825f846047ecc37b661f2fd95e67
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-22 18:31:32 -0400

    feat: Add Footer component

---
+++ Client/src/components/footer/index.jsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <div className=" mt-60">
+      <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+        <nav className="grid grid-flow-col gap-4">
+          <Link to={'/about-me'} className="link link-hover">
+            About me
+          </Link>
+
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            to="https://docs.google.com/document/d/18ySfM4Db26B9PCVPvKZI5ISMoRCcCt7BLqKDb1_F9HY/edit?usp=sharing"
+            className="link link-hover"
+          >
+            Resume
+          </Link>
+
+          <Link to={'contact'} className="link link-hover">
+            Contact
+          </Link>
+
+          <Link to={'donate'} className="link link-hover">
+            Donate
+          </Link>
+        </nav>
+
+        <nav>
+          <div className="grid grid-flow-col gap-4"></div>
+        </nav>
+
+        <aside>
+          <p>Copyright © 2024 - All right reserved by Cool Games Ltd</p>
+        </aside>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
