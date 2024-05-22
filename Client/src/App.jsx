21fd0d437ea9fe86fcac7a10d4e3201ad133fea7
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-22 18:30:25 -0400

    refactor: Update App component to include Footer and react router

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,16 @@
-import Hero from './components/hero section/hero';
+// component for the main app
 import Navbar from './components/navbar';
+import Footer from './components/footer';
+
+// react router
+import { Outlet } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Outlet />
+      <Footer />
     </>
   );
 }
