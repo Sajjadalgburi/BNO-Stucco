c4e2909a4d343d640768855f900a8c741928d421
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-22 18:31:15 -0400

    feat: Add homepage component with Hero and MaterialInfo sections

---
+++ Client/src/pages/homepage.jsx
@@ -0,0 +1,15 @@
+// this is the root homepage component that will have all the other components in it
+
+import Hero from '../components/hero section/hero';
+import MaterialInfo from '../components/material Info';
+
+const Homepage = () => {
+  return (
+    <div>
+      <Hero />
+      <MaterialInfo />
+    </div>
+  );
+};
+
+export default Homepage;
