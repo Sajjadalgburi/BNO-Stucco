0ea752f100b5e5e6d91e41a795db6a23402b475e
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-23 19:08:22 -0400

    refactor: Update hero section to include welcome message and improve content readability

--- Client/src/components/hero section/hero.jsx
+++ Client/src/components/hero section/hero.jsx
@@ -4,11 +4,16 @@ export default function Hero() {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          <h1 className="mb-5 text-5xl font-bold uppercase welcome">
+            Welcome!
+          </h1>
+          <p className="mb-5 text-lg font-normal">
+            Brothers Exterior stands as a cherished privately owned family
+            business, passionately dedicated to delivering unparalleled house
+            stucco services across the Greater Toronto Area (GTA). With a
+            steadfast commitment to excellence, we ensure each project receives
+            the highest quality craftsmanship and attention to detail, turning
+            houses into cherished homes.
           </p>
         </div>
       </div>
