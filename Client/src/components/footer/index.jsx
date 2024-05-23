e986486a0615ae83bd859b8af6558599b275b5e4
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-23 19:08:15 -0400

    refactor: Update footer component to include Instagram link

--- Client/src/components/footer/index.jsx
+++ Client/src/components/footer/index.jsx
@@ -1,34 +1,29 @@
 import { Link } from 'react-router-dom';
 
+import Instagram from '../links/Instagram';
+
 const Footer = () => {
   return (
     <div className=" mt-60">
       <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
         <nav className="grid grid-flow-col gap-4">
           <Link to={'/about-me'} className="link link-hover">
-            About me
-          </Link>
-
-          <Link
-            target="_blank"
-            rel="noopener noreferrer"
-            to="https://docs.google.com/document/d/18ySfM4Db26B9PCVPvKZI5ISMoRCcCt7BLqKDb1_F9HY/edit?usp=sharing"
-            className="link link-hover"
-          >
-            Resume
+            About us
           </Link>
 
           <Link to={'contact'} className="link link-hover">
             Contact
           </Link>
 
-          <Link to={'donate'} className="link link-hover">
-            Donate
+          <Link to={'phone'} className="link link-hover">
+            Phone
           </Link>
         </nav>
 
         <nav>
-          <div className="grid grid-flow-col gap-4"></div>
+          <div className="grid grid-flow-col gap-4">
+            <Instagram />
+          </div>
         </nav>
 
         <aside>
