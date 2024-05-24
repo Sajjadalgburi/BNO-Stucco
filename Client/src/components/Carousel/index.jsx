0a1474576868a97fb5b4994881f54d97c0dc928e
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-24 15:41:31 -0400

    feat: Add HousesCarousel component for displaying house images in a carousel

---
+++ Client/src/components/Carousel/index.jsx
@@ -0,0 +1,73 @@
+const HousesCarousel = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="carousel w-8/12 md:w-5/12">
+        <div
+          id="slide1"
+          className="HousesCarousel carousel-item relative w-full"
+        >
+          <img
+            src="../images/house5.jpg"
+            alt="house image"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img
+            src="../images/house6.jpg"
+            alt="house image"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img
+            src="../images/house9.jpg"
+            alt="house image"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img
+            src="../images/house10.jpg"
+            alt="house image"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HousesCarousel;
