741c28d7046973e568d884548fb37b50b10dcd57
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-24 15:19:19 -0400

    refactor: Update ServicesProvided component to improve image layout and responsiveness

--- Client/src/components/servicesProvided/index.jsx
+++ Client/src/components/servicesProvided/index.jsx
@@ -1,11 +1,11 @@
 const ServicesProvided = () => {
   return (
     <div className="service my-44">
-      <div className="description">
-        <h1 className="text-5xl font-bold flex justify-center align-middle uppercase">
+      <div className="service">
+        <h1 className="text-4xl sm:text-6xl font-bold flex justify-center align-middle uppercase">
           Services we provide
         </h1>
-        <p className="mt-4 w-auto text-center mx-24 text-lg">
+        <p className="mt-4 w-auto text-center mx-8 md:mx-24 text-md md:text-xl">
           Elevate your exterior with Brothers Exterior Stucco, commitment to
           quality craftsmanship and client satisfaction. With expertise in
           expert installation using premium materials from brands like ADEX and
@@ -16,35 +16,18 @@ const ServicesProvided = () => {
           firsthand.
         </p>
       </div>
-      {/* this is the gird for the images  */}
-      <div className="imagesForHouseWork grid grid-cols-1 md:grid-cols-2 gap-10 justify-center items-center mt-10">
-        <div className="house" aria-label="Image for house">
-          <img
-            src="../images/house2.jpg"
-            alt="house"
-            className="max-w-xs md:max-w-md object-contain"
-          />
+      <div className="avatar flex flex-wrap justify-center items-center mt-10 gap-4">
+        <div className="w-96 rounded m-2 flex-grow flex-shrink">
+          <img src="../images/house2.jpg" alt="house image" />
         </div>
-        <div className="house" aria-label="Image for house">
-          <img
-            src="../images/house1.jpg"
-            alt="house"
-            className="max-w-xs md:max-w-md object-contain"
-          />
+        <div className="w-96 rounded m-2 flex-grow flex-shrink">
+          <img src="../images/house3.jpg" alt="house image" />
         </div>
-        <div className="house" aria-label="Image for house">
-          <img
-            src="../images/house3.jpg"
-            alt="house"
-            className="max-w-xs md:max-w-md object-contain"
-          />
+        <div className="w-96 rounded m-2 flex-grow flex-shrink">
+          <img src="../images/house1.jpg" alt="house image" />
         </div>
-        <div className="house" aria-label="Image for house">
-          <img
-            src="../images/house4.jpg"
-            alt="house"
-            className="max-w-xs md:max-w-md object-contain"
-          />
+        <div className="w-96 rounded m-2 flex-grow flex-shrink">
+          <img src="../images/house4.jpg" alt="house image" />
         </div>
       </div>
     </div>
