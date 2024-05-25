3cdb6139c6ed156603e8b4e42c85857e4a132682
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-25 18:33:07 -0400

    refactor: Update footer component with responsive layout and social media links

--- Client/src/components/footer/index.jsx
+++ Client/src/components/footer/index.jsx
@@ -1,34 +1,28 @@
 import { Link } from 'react-router-dom';
-
 import Instagram from '../links/Instagram';
 import FaceBook from '../links/Facebook';
 
 const Footer = () => {
   return (
-    <div className=" mt-60">
+    <div className="mt-60">
       <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
         <nav className="grid grid-flow-col gap-4">
-          <Link to={'/about-me'} className="link link-hover">
+          <a href="#about-us" className="link link-hover">
             About us
-          </Link>
-
-          <Link to={'contact'} className="link link-hover">
+          </a>
+          <Link to="/contact" className="link link-hover">
             Contact
           </Link>
-
-          <Link to={'phone'} className="link link-hover">
+          <a href="tel:+4168366130" className="link link-hover">
             Phone
-          </Link>
+          </a>
         </nav>
-
         <nav>
           <div className="grid grid-flow-col gap-4">
             <Instagram />
-
             <FaceBook />
           </div>
         </nav>
-
         <aside>
           <p>Copyright © 2024 - All right reserved by BNO Ltd</p>
         </aside>
