a41f3480fa5bade3af0f1c0e28609c4c7638d815
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-25 18:33:22 -0400

    refactor: Update navbar component to include phone number link

--- Client/src/components/navbar/index.jsx
+++ Client/src/components/navbar/index.jsx
@@ -26,7 +26,9 @@ export default function Navbar() {
             className="text-lg uppercase font-medium menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <Link>Phone Us</Link>
+              <a href="tel:+4168366130" className="link link-hover">
+                Phone
+              </a>{' '}
             </li>
 
             <li>
