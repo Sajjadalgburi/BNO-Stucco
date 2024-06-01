627d5f03a1daf176a596c1730c6d181c97a23e25
Sajjadalgburi <dfgsajjad@gmail.com>
2024-06-01 19:55:14 -0400

    refactor: Update MongoDB connection configuration

--- Server/config/dbConnection.js
+++ Server/config/dbConnection.js
@@ -1,9 +1,11 @@
 // this file is responsible for creating a connection to the mongoDb database
 
-const { connection, connect } = require("mongoose");
+require('dotenv').config();
+
+const { connection, connect } = require('mongoose');
 
 // creating the connection string to the database
-connect(process.env.mongoUri || "mongodb://localhost:27017/BNOStucco");
+connect(process.env.mongoUri || 'mongodb://localhost:27017/BNOStucco');
 
 // exporting the connection
 module.exports = connection;
