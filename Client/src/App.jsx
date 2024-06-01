147c7999d782975eb9773c3dfe8ff6ea0b1bcce8
Sajjadalgburi <dfgsajjad@gmail.com>
2024-06-01 16:37:35 -0400

    refactor: Update ApolloProvider in App component to use ApolloClient from @apollo/client

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,15 +2,33 @@
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 
+// apollo client for graphql
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 // react router
 import { Outlet } from 'react-router-dom';
 
 export default function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
       <Footer />
-    </>
+    </ApolloProvider>
   );
 }
