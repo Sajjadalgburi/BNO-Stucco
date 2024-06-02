511c405d527c6e478b0407172c540bc64d62d14e
Sajjadalgburi <dfgsajjad@gmail.com>
2024-06-01 20:12:11 -0400

    refactor: Update import path for HousesCarousel component

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,12 @@
 // this is the main backend file that will run the server and connect to the database
 
-const express = require("express");
-const { ApolloServer } = require("@apollo/server");
-const path = require("path");
-const db = require("./config/dbConnection");
-const { expressMiddleware } = require("@apollo/server/express4");
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const path = require('path');
+const db = require('./config/dbConnection');
+const { expressMiddleware } = require('@apollo/server/express4');
 
-const { typeDefs, resolvers } = require("./schema");
+const { typeDefs, resolvers } = require('./schema');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -25,19 +25,19 @@ const startApolloServer = async () => {
     // Applying required middleware for the application
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
-    app.use("/graphql", expressMiddleware(server));
+    app.use('/graphql', expressMiddleware(server));
 
     // Serving different files based on the environment
-    if (process.env.NODE_ENV === "production") {
-      app.use(express.static(path.join(__dirname, "../client/dist")));
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../Client/dist')));
 
-      app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+      app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../Client/dist/index.html'));
       });
     }
 
     // Connect to the database and start the Express.js server
-    db.once("open", () => {
+    db.once('open', () => {
       app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
         console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
@@ -45,8 +45,8 @@ const startApolloServer = async () => {
     });
   } catch (error) {
     console.error(
-      "Error starting the Apollo Server or connecting to the database:",
-      error
+      'Error starting the Apollo Server or connecting to the database:',
+      error,
     );
   }
 };
