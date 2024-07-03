65082923719790325b162c3b61f9a61549311985
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-03 12:36:44 -0600

    Create TypeDef strecuture for the images from aws s3 bucket

--- Server/schema/typeDefs.js
+++ Server/schema/typeDefs.js
@@ -1,5 +1,5 @@
 // Import the gql tag from graphql-tag
-const gql = require("graphql-tag");
+const gql = require('graphql-tag');
 
 // typeDefs for the contact form
 const typeDefs = gql`
@@ -18,9 +18,17 @@ const typeDefs = gql`
     message: String!
   }
 
+  type AWS_S3Bucket_Imgs {
+    ETag: ID!
+    ImgURL: String!
+  }
+
   ## this is a placeholder type to satisfy the GQL server
   type Query {
     _empty: String
+    getHeroImages: [AWS_S3Bucket_Imgs]
+    getHouseImages: [AWS_S3Bucket_Imgs]
+    getMaterialImages: [AWS_S3Bucket_Imgs]
   }
 
   type Mutation {
